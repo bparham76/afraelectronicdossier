6480488ee356c23ec949dd132944ec29fe15c555
Parham Baghizadeh <parham.baghizadeh@yahoo.com>
2024-03-07 15:51:13 +0330

    fix: login page

--- app/src/pages/Authentication.jsx
+++ app/src/pages/Authentication.jsx
@@ -1,6 +1,10 @@
-import { Typography, Box, Card, TextField, Button } from '@mui/material';
+import { Typography, Box, Stack, Card, TextField, Button } from '@mui/material';
+import { useState, useEffect } from 'react';
 
 const Authentication = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
 	return (
 		<Box
 			sx={{
@@ -10,12 +14,36 @@ const Authentication = () => {
 				alignItems: 'center',
 				justifyContent: 'center',
 			}}>
-			<Card sx={{ padding: 2 }}>
+			<Card
+				elevation={4}
+				sx={{ padding: 4, borderRadius: 4 }}>
 				<Typography
+					mb={4}
 					variant='h4'
 					fontWeight='bold'>
 					ورود به سیستم
 				</Typography>
+				<Stack gap={1}>
+					<TextField
+						value={username}
+						onChange={e => setUsername(e.target.value)}
+						size='small'
+						type='text'
+						label='نام کاربری'
+					/>
+					<TextField
+						value={password}
+						onChange={e => setPassword(e.target.value)}
+						size='small'
+						type='password'
+						label='رمز عبور'
+					/>
+					<Button
+						disabled={username.length == 0 || password.length == 0}
+						variant='contained'>
+						ورود
+					</Button>
+				</Stack>
 			</Card>
 		</Box>
 	);
